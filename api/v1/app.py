434791bd4af3aa93d9cdf62fdfa17898685c77c6
root <hanichris71@gmail.com>
2022-11-21 15:55:09 +0000

    Created a handler for 404 errors. Returns a JSON-formatted 404 status code.

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """Start of the Flask API."""
 from api.v1.views import app_views
-from flask import Flask
+from flask import Flask, jsonify
 from models import storage
 from os import getenv
 
@@ -12,6 +12,10 @@ app.register_blueprint(app_views)
 @app.teardown_appcontext
 def teardown_storage(self):
     storage.close()
+
+@app.errorhandler(404)
+def resource_not_found(e):
+    return jsonify({'error': 'Not found'}), 404
 
 if __name__ == "__main__":
     HBNB_API_HOST = getenv('HBNB_API_HOST')
